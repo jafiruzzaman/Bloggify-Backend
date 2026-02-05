db3a266c3fa79b9b0c628aac2d6c5e849baf7c2d
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-02-05 16:38:53 +0600

    feat: create blog interface

---
+++ src/interface/blog.interface.ts
@@ -0,0 +1,21 @@
+/**
+ * @copyright 2026 Mohammad Jafiruzzaman
+ */
+
+/*============================================== Node Modules ============================================== */
+import type { Document, Types } from 'mongoose';
+/*============================================== Export IBlog ============================================== */
+
+export interface IBlog extends Document {
+	id: Types.ObjectId;
+	title: string;
+	slug: string;
+	coverImage: string;
+	content: string;
+	author: Types.ObjectId;
+	likesCount: number;
+	commentsCount: number;
+	status: 'published' | 'draft';
+	createdAt: Date;
+	updatedAt: Date;
+}
