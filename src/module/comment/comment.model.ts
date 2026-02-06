22a7fd923c82ba93fdd210299eb4dd9686f9e04d
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-02-06 17:02:39 +0600

    feat: create comment interface and model

---
+++ src/module/comment/comment.model.ts
@@ -0,0 +1,33 @@
+/**
+ * @copyright 2026 Mohammad Jafiruzzaman
+ */
+
+/*============================================== Node Modules ============================================== */
+import { Schema, model, Model, Types } from 'mongoose';
+
+/*============================================== Custom Modules ============================================== */
+import type { IComment } from '@interface/comment.interface.ts';
+
+const commentSchema: Schema<IComment> = new Schema<IComment>(
+	{
+		author: {
+			type: Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		blog: {
+			type: Types.ObjectId,
+			ref: 'Blog',
+			required: true,
+		},
+		content: {
+			type: String,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+/*============================================== Export ============================================== */
+export const CommentModel: Model<IComment> = model('Comment', commentSchema);
